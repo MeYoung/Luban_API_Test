6b9e01fdb238ffb2c7b8d882bb4fc18f46e902e0
shijin.huang <shijin.huang@luckincoffee.com>
2020-12-22 14:16:07 +0800

    test demo

--- src/test/java/otp/demo/OtpDemoTest.java
+++ src/test/java/otp/demo/OtpDemoTest.java
@@ -10,7 +10,7 @@ public class OtpDemoTest {
 
     @BeforeSuite(alwaysRun = true)
     @Parameters({"env"})
-    public void beforeTest(String env) {
+    public void beforeTest2(String env) {
         System.err.println("evn:" + env);
         BaseData baseData = new BaseData();
         baseData.setEnv(env);
@@ -19,30 +19,50 @@ public class OtpDemoTest {
     }
 
 
-    @Test(description = "表格的测试用例1", groups = {"tttttt","1_205_766_c4674mxvp6g0","1_205_766_c4674ncx5gw0","123","456"})
-    public void test1() {
+    @Test(description = "表格的测试用例1", groups = "1_441_21672_c7t89zf3xxk0")
+    public void test1() throws InterruptedException {
+        Thread.sleep(1000);
         System.out.println("test1.....表格的测试用例1");
     }
 
-    @Test(description = "脑图的测试用例2", groups = "2_205_25_6875256808567668737")
-    public void test2() {
-        AssertUtils.assertEquals(1,2);
+    @Test(description = "脑图的测试用例2", groups = {"2_83_29_6860729584200384544", "fadsfasf"})
+    public void test2() throws InterruptedException {
+        Thread.sleep(2000);
+        AssertUtils.assertEquals(1, 2);
         System.out.println("test2.....表格的测试用例2");
     }
 
-    @Test(description = "脑图的测试用例3", groups = {"1_205_804_c5to0mlnqbs0","ttt11123123ttt","1_205_804_c5to0mywwy00","1_205_804_c5to0ncdj140"})
-    public void test3() {
+    @Test(description = "脑图的测试用例3-TEST", groups = {"1_205_932_c7z38ufv5140"})
+    public void test3() throws InterruptedException {
+        Thread.sleep(3000);
+
         System.out.println("test3.....脑图的测试用例3");
     }
 
-    @Test(description = "脑图的测试用例4", groups = "1_205_804_c5to0r9ql6w0")
-    public void test4() {
+    @Test(description = "脑图的测试用例4-TEST", groups = "1_205_932_c7z38t4luao0")
+    public void test4() throws InterruptedException {
+        Thread.sleep(4000);
         System.out.println("test4.....脑图的测试用例4");
     }
 
 
+    @DataProvider(name = "loginData")
+    public Object[][] loginData() {
+        return new Object[][]{
+                {"X", "pwd1"},
+                {"n2", "p2"},
+                {"n3", "p3"}
+        };
+    }
+
+    @Test(dataProvider = "loginData", dependsOnGroups = "脑图的测试用例5-TEST", groups = {"1_205_932_c7z38ra89i80"})
+    public void tlogin(String userName, String pwd) {
+        System.out.println("userName:" + userName + ",PassWord:" + pwd);
+    }
+
+
     @AfterSuite(alwaysRun = true)
-    public void afterTest() {
+    public void afterTest2() {
         System.err.println("after test .......");
     }
 
