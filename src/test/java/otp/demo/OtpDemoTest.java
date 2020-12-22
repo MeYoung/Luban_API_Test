c43ca3dd1ea573132c9ceab0c06bc0dc2c256b6d
shijin.huang <shijin.huang@luckincoffee.com>
2020-12-22 14:44:48 +0800

    规范

--- src/test/java/otp/demo/OtpDemoTest.java
+++ src/test/java/otp/demo/OtpDemoTest.java
@@ -3,46 +3,48 @@ package otp.demo;
 import com.lucky.common.utils.AssertUtils;
 import com.lucky.common.utils.BaseDataUtils;
 import com.lucky.params.BaseData;
+import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.*;
 
+@Slf4j
 public class OtpDemoTest {
 
 
     @BeforeSuite(alwaysRun = true)
     @Parameters({"env"})
     public void beforeTest2(String env) {
-        System.err.println("evn:" + env);
+        log.info(env);
         BaseData baseData = new BaseData();
         baseData.setEnv(env);
         BaseDataUtils.setBaseData(baseData);
-        System.err.println("before test......");
+        log.info("before test......");
     }
 
 
     @Test(description = "表格的测试用例1", groups = "1_441_21672_c7t89zf3xxk0")
     public void test1() throws InterruptedException {
         Thread.sleep(1000);
-        System.out.println("test1.....表格的测试用例1");
+        log.info("test1.....表格的测试用例1-1_441_21672_c7t89zf3xxk0");
     }
 
     @Test(description = "脑图的测试用例2", groups = {"2_83_29_6860729584200384544", "fadsfasf"})
     public void test2() throws InterruptedException {
         Thread.sleep(2000);
         AssertUtils.assertEquals(1, 2);
-        System.out.println("test2.....表格的测试用例2");
+        log.info("脑图的测试用例2-2_83_29_6860729584200384544");
     }
 
     @Test(description = "脑图的测试用例3-TEST", groups = {"1_205_932_c7z38ufv5140"})
     public void test3() throws InterruptedException {
         Thread.sleep(3000);
 
-        System.out.println("test3.....脑图的测试用例3");
+        log.info("test3.....脑图的测试用例3-1_205_932_c7z38ufv5140");
     }
 
     @Test(description = "脑图的测试用例4-TEST", groups = "1_205_932_c7z38t4luao0")
     public void test4() throws InterruptedException {
         Thread.sleep(4000);
-        System.out.println("test4.....脑图的测试用例4");
+        log.info("test4.....脑图的测试用例4-1_205_932_c7z38t4luao0");
     }
 
 
@@ -57,13 +59,13 @@ public class OtpDemoTest {
 
     @Test(dataProvider = "loginData", description = "脑图的测试用例5-TEST", groups = {"1_205_932_c7z38ra89i80"})
     public void tlogin(String userName, String pwd) {
-        System.out.println("userName:" + userName + ",PassWord:" + pwd);
+        log.info("userName:" + userName + ",PassWord:" + pwd);
     }
 
 
     @AfterSuite(alwaysRun = true)
     public void afterTest2() {
-        System.err.println("after test .......");
+        log.error("after test .......");
     }
 
 }
