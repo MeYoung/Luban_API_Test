6d90b2a4ae64c61441f0c0ddea362a516161b3fc
shijin.huang <shijin.huang@luckincoffee.com>
2021-10-09 11:34:42 +0800

    debug

--- src/main/java/com/lucky/common/annotion/CasePriority.java
+++ src/main/java/com/lucky/common/annotion/CasePriority.java
@@ -9,7 +9,7 @@ import io.qameta.allure.SeverityLevel;
 public enum CasePriority {
 
 //    P0("blocker"),
-    P0(SeverityLevel.BLOCKER.value()),
+    P0("P0"),
     P1(SeverityLevel.CRITICAL.value()),
     P2(SeverityLevel.NORMAL.value()),
     P3(SeverityLevel.MINOR.value());
